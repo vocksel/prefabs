9f6f4859b62372825a230135acf9aff8d7422b34
David Minnerly <voxeldavid@gmail.com>
2018-08-11 22:55:34 -0700

    Stop logging state

--- src/store.lua
+++ src/store.lua
@@ -3,6 +3,6 @@ local root = script.Parent
 local Rodux = require(root.lib.Rodux)
 local reducer = require(root.reducer)
 
-local store = Rodux.Store.new(reducer, nil, { Rodux.loggerMiddleware, Rodux.thunkMiddleware })
+local store = Rodux.Store.new(reducer, nil, { Rodux.thunkMiddleware })
 
 return store
